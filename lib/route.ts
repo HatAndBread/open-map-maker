eb6a03cf4748525dfffbcfa9f5746c7b4518eb01
HatAndBread <61222965+HatAndBread@users.noreply.github.com>
2023-12-31 11:33:29 +0900

    Update running line color

--- lib/route.ts
+++ lib/route.ts
@@ -168,7 +168,7 @@ export default class Route {
     if (!this.map) return
     if (this.runningLine) this.map.removeLayer(this.runningLine);
     this.runningLine?.remove()
-    this.runningLine = L.polyline(this.runningLatLngs, { color: "rgba(0,10,150,0.5)" });
+    this.runningLine = L.polyline(this.runningLatLngs, { color: "rgba(103,232,249,0.4)" });
     this.runningLine.addTo(this.map);
   }
 
